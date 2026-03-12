e6e5c5242f852849b27238a9ea3693d935e6436d
Lukas W <lukaswhl@gmail.com>
2026-03-12 11:42:54 +0100

    Improve IP address space detection

--- src/address-space.test.js
+++ src/address-space.test.js
@@ -1,6 +1,42 @@
 import {describe, expect, test} from 'vitest';
 
-import {isLessPublic} from "./address-space";
+import {guessAddressSpace, isLessPublic} from "./address-space";
+
+// Mirroring IP address test cases from firefox source
+describe('guessAddressSpace', () => {
+	describe('loopback', () => {
+		test.each([
+			'127.0.0.1',
+			'0:0:0:0:0:0:0:1', '::1', '[::1]',
+			'::ffff:127.0.0.1', // IPv4-mapped IPv6 address
+			'localhost', 'myapp.localhost',
+		])('%s', (address) => {
+			expect(guessAddressSpace(address)).toBe('loopback');
+		})
+	})
+	describe('local', () => {
+		test.each([
+			'10.0.0.1',
+			'100.64.0.1', '100.127.255.254', '172.16.0.1', '172.31.255.255',
+			'192.168.1.1',
+			'169.254.0.1', '169.254.255.254',
+			'fe80::1',
+			'fc00::', 'fdff:ffff:ffff:ffff:ffff:ffff:ffff:ffff',  // Unique local
+			'::ffff:10.0.0.1', // IPv4-mapped IPv6
+		])('%s', (address) => {
+			expect(guessAddressSpace(address)).toBe('local');
+		})
+	})
+	describe('public', () => {
+		test.each([
+			'8.8.8.8', '1.1.1.1',
+			'2001:4860:4860::8888', '2606:4700:4700::1111',
+			'::ffff:1.1.1.1', // IPv4-mapped IPv6
+		])('%s', (address) => {
+			expect(guessAddressSpace(address)).toBe('public');
+		})
+	})
+})
 
 describe('isLessPublic', () => {
 	test.each([
--- src/address-space.ts
+++ src/address-space.ts
@@ -1,3 +1,5 @@
+import {Address4, Address6} from 'ip-address';
+
 type AddressSpace = "loopback" | "local" | "public" | "unknown";
 
 /**
@@ -9,17 +11,51 @@ type AddressSpace = "loopback" | "local" | "public" | "unknown";
  * * @param {string} hostname - Hostname to check
  */
 export function guessAddressSpace(hostname: string): AddressSpace {
-    const host = hostname.toLowerCase();
+    let host = hostname.toLowerCase();
 
-    if (host.indexOf('localhost') > -1 ||
-        host === '::1' ||
-        host === '[::1]' ||
-        host.indexOf('127.') === 0
-    ) {
-        return "loopback"
+    // Remove IPv6 host brackets
+    if (host.match('\[[0-9a-fA-f:]+\]')) {
+        host = host.slice(1, -1);
+    }
+    if (Address6.isValid(host)) return getIp6AddressSpace(new Address6(host));
+    if (Address4.isValid(host)) return getIp4AddressSpace(new Address4(host));
+
+    if (host.indexOf('localhost') !== -1) {
+        return "loopback";
     }
 
     return "local";
+}
+
+function getIp4AddressSpace(ip: Address4): AddressSpace {
+    // Loopback addresses
+    if (ip.isInSubnet(new Address4('127.0.0.0/8'))) return "loopback";
+    // Class A networks
+    if (ip.isInSubnet(new Address4('10.0.0.0/8'))) return "local";
+    // Class B networks
+    if (ip.isInSubnet(new Address4('172.16.0.0/12'))) return "local";
+    // Class C networks
+    if (ip.isInSubnet(new Address4('192.168.0.0/16'))) return "local";
+    // DHCP
+    if (ip.isInSubnet(new Address4('169.254.0.0/16'))) return "local";
+    // Carrier-grade NAT
+    if (ip.isInSubnet(new Address4('100.64.0.0/10'))) return "local";
+    return "public";
+}
+
+function getIp6AddressSpace(ip: Address6): AddressSpace {
+    if (ip.isLoopback()) return "loopback";
+    if (ip.isLinkLocal()) return "local";
+    if (ip.isInSubnet(new Address6('fc00::/7'))) return "local";
+    // IPv4-mapped IPv6
+    if (ip.isInSubnet(new Address6('::ffff:0:0/96'))) {
+        const ipv4 = Address4.fromHex(ip.getBitsBase16(96, 128));
+        return getIp4AddressSpace(ipv4);
+    }
+    if (ip.is6to4()) {
+        return getIp4AddressSpace(ip.inspect6to4().gateway);
+    }
+    return "public";
 }
 
 // Assumes that loopback is always detected, i.e. "unknown" can't include "loopback"
