443b67de16d908ce791b2bfbc65401c5d4bd0999
Lukas W <lukaswhl@gmail.com>
2026-03-12 10:58:54 +0100

    Rework isLnaAllowed

--- src/index.js
+++ src/index.js
@@ -1,57 +1,14 @@
-import {guessAddressSpace} from "./address-space";
+import {getRequiredPermission} from "./permissions";
 
-async function isLnaAllowed(hostname, debug) {
-	// first, query the old property
-	const state = await lnaPermissionQuery();
-	switch (state) {
-		case "granted":
-			console.log("Not granted. State:", state);
-			return true;
-		case "unknown":
-			console.log("Unknown, we'll look deeper...");
-			var addressSpace = guessAddressSpace(hostname);
-			// second, query the "scoped" property
-			const scopedState = await lnaPermissionQuery(addressSpace);
-			switch (scopedState) {
-				case "granted":
-					console.log("Granted. State:", state);
-					return true;
-				default:
-					console.log("Not granted. State:", state);
-					return false;
-			}
-		default:
-			console.log("Not granted. State:", state);
-			return false;
-	}
-}
+async function wasLnaDenied(hostname) {
+	const permission = getRequiredPermission(hostname);
+	if (permission === null) return false;
+	if (permission === undefined) return undefined;
 
-
-/**
- * prompt: User hasn't accepted it yet
- * granted: User has accepted it (if we're using this retroactively, this should never happen)
- * denied: Help inform the user for appropriate action
- * unknown: Our own magic value to look deeper and eventually give up
- */
-async function lnaPermissionQuery(addressSpace) {
-	const name = {name: 'local-network-access'};
-	if (addressSpace) {
-		name.name = addressSpace + "-network";
-	}
-	if (typeof navigator !== 'undefined' && navigator.permissions !== 'undefined') {
-		try {
-			return (await navigator.permissions.query(name)).state;
-		} catch (err) {
-			if (err instanceof TypeError) {
-				console.log("Permission", name.name, "is not supported by this browser");
-			} else {
-				console.error(err);
-			}
-			return "unknown";
-		}
-	} else {
-		return Promise.resolve("unknown");
-	}
+	const state = (await navigator.permissions.query(permission)).state;
+	if (state === "denied") return true;
+	if (state === "granted") return false;
+	return undefined;
 }
 
 class LnaDeniedError extends Error {
@@ -65,12 +22,12 @@ async function fetchLna(url) {
 	try {
 		return await fetch(url);
 	} catch (e) {
-		if (await isLnaAllowed(new URL(url).hostname, true)) {
-			throw e;
-		} else {
+		if (await wasLnaDenied(new URL(url).hostname, true)) {
 			// TODO: We're in a 'prompt' or 'blocked' scenario, add callback to try fetch again
 			// when this status changes
 			throw new LnaDeniedError();
+		} else {
+			throw e;
 		}
 	}
 }
---
+++ src/permissions.ts
@@ -0,0 +1,49 @@
+import {guessAddressSpace, isLessPublic} from "./address-space.js";
+
+const LnaJointPermission = 'local-network-access';
+const LnaLoopbackPermission = 'loopback-network';
+const LnaLocalPermission = 'local-network';
+
+export type LnaPermissionName =
+    typeof LnaJointPermission
+    | typeof LnaLoopbackPermission
+    | typeof LnaLocalPermission;
+
+async function permissionSupported(name: PermissionName | LnaPermissionName): Promise<boolean> {
+    if (!navigator?.permissions?.query) return false;
+    try {
+        await navigator.permissions.query({name} as { name: PermissionName });
+        return true;
+    } catch (err) {
+        if (err instanceof TypeError) {
+            return false;
+        } else {
+            throw err;
+        }
+    }
+}
+
+const LnaPermissionNames: LnaPermissionName[] = [
+    LnaLoopbackPermission, LnaLocalPermission, LnaJointPermission,
+];
+
+const SupportedPermissions = Object.fromEntries(
+    await Promise.all(LnaPermissionNames.map(name => [name, permissionSupported(name)]))
+);
+export const SplitPermissionsSupported = SupportedPermissions[LnaLoopbackPermission] && SupportedPermissions[LnaLocalPermission];
+export const JointPermissionSupported = SupportedPermissions[LnaJointPermission];
+export const LnaPermissionsSupported = SplitPermissionsSupported || JointPermissionSupported;
+
+export function getRequiredPermission(hostname: string) {
+    const targetSpace = guessAddressSpace(hostname);
+    const originSpace = guessAddressSpace(window.location.hostname);
+    const lessPublic = isLessPublic(targetSpace, originSpace);
+
+    if (lessPublic === false) return null;
+    if (lessPublic === undefined || !LnaPermissionsSupported) return undefined;
+
+    if (!SplitPermissionsSupported) return LnaJointPermission;
+    if (targetSpace === "loopback") return LnaLoopbackPermission;
+    if (targetSpace === "local") return LnaLoopbackPermission;
+    return undefined;
+}
