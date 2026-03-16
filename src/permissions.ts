qzind/lna

commit 0fe729b0d0d0db1b5025df87d91f05e91c94a85b

Fix required permission detection, add test

src/permissions.ts
@@ -1,4 +1,4 @@
-import {guessAddressSpace, isLessPublic} from "./address-space.js";
+import {AddressSpace, guessAddressSpace, isLessPublic} from "./address-space.js";
 
 const LnaJointPermission = 'local-network-access';
 const LnaLoopbackPermission = 'loopback-network';
@@ -34,18 +34,23 @@ export const SplitPermissionsSupported = SupportedPermissions[LnaLoopbackPermiss
 export const JointPermissionSupported = SupportedPermissions[LnaJointPermission];
 export const LnaPermissionsSupported = SplitPermissionsSupported || JointPermissionSupported;
 
-export function getRequiredPermission(hostname: string) {
-	const targetSpace = guessAddressSpace(hostname);
-	const originSpace = guessAddressSpace(window.location.hostname);
+export function getRequiredPermissionForAddressSpaces(targetSpace: AddressSpace, originSpace: AddressSpace) {
 	const lessPublic = isLessPublic(targetSpace, originSpace);
 
-	if (lessPublic === false) return null;
-	if (lessPublic === undefined || !LnaPermissionsSupported) return undefined;
+	if (lessPublic === false || !LnaPermissionsSupported) return null;
+	if (lessPublic === undefined) return undefined;
 
 	if (!SplitPermissionsSupported) return LnaJointPermission;
 	if (targetSpace === "loopback") return LnaLoopbackPermission;
-	if (targetSpace === "local") return LnaLoopbackPermission;
+	if (targetSpace === "local") return LnaLocalPermission;
 	return undefined;
+}
+
+export function getRequiredPermission(hostname: string) {
+	return getRequiredPermissionForAddressSpaces(
+		guessAddressSpace(hostname),
+		guessAddressSpace(window.location.hostname)
+	)
 }
 
 export async function getLnaPermission(name: LnaPermissionName) {

src/permissions.browser.test.js
@@ -1,6 +1,12 @@
 import {describe, expect, test} from 'vitest'
 import {commands} from 'vitest/browser';
-import {getLnaPermissionState, SupportedPermissions} from "./permissions";
+import {
+	getLnaPermissionState,
+	getRequiredPermissionForAddressSpaces,
+	LnaPermissionsSupported,
+	SplitPermissionsSupported,
+	SupportedPermissions
+} from "./permissions";
 import Bowser from 'bowser';
 
 if (typeof window === 'undefined') {
@@ -40,9 +46,40 @@ describe('SupportedPermissions', () => {
 if (supported.length) {
 	describe('getLnaPermissionState', async () => {
 		test.each(supported)('%s', async name => {
+			await commands.setPermissions({name}, 'prompt');
 			expect(await getLnaPermissionState(name)).toEqual('prompt');
 			await commands.setPermissions({name}, 'granted');
 			expect(await getLnaPermissionState(name)).toEqual('granted');
 		})
 	})
 }
+
+describe('getRequiredPermissionForAddressSpaces', () => {
+	let cases = [
+		['public', 'public', null],
+		['public', 'local', 'local-network'],
+		['public', 'loopback', 'loopback-network'],
+		['public', 'unknown', undefined],
+		['local', 'public', null],
+		['local', 'local', null],
+		['local', 'loopback', 'loopback-network'],
+		['local', 'unknown', undefined],
+		['loopback', 'public', null],
+		['loopback', 'local', null],
+		['loopback', 'loopback', null],
+		['loopback', 'unknown', null],
+		['unknown', 'public', null],
+		['unknown', 'local', undefined],
+		['unknown', 'loopback', 'loopback-network'], // Assuming 'unknown' can't be 'loopback'
+		['unknown', 'unknown', undefined],
+	];
+	if (!LnaPermissionsSupported) {
+		cases = cases.map(([from, to]) => [from, to, null]);
+	} else if (!SplitPermissionsSupported) {
+		cases = cases.map(([from, to, p]) => [from, to, p ? 'local-network-access' : p]);
+	}
+
+	test.each(cases)('from `%s` to `%s` requires `%s` permission', (from, to, expected) => {
+		expect(getRequiredPermissionForAddressSpaces(to, from)).toStrictEqual(expected);
+	})
+});

commit 70c689970add1a6569efb2b85a19a6b4801dd9fb

Fix getRequiredPermission argument order
Regression from 0fe729b0d0d0db1b5025df87d91f05e91c94a85b

src/permissions.ts
@@ -49,8 +49,8 @@ export function getRequiredPermissionForAddressSpaces(targetSpace: AddressSpace,
 
 export function getRequiredPermission(hostname: string, overrides?: AddressSpaceOverrides) {
 	return getRequiredPermissionForAddressSpaces(
+		overrides?.targetAddressSpace ?? guessAddressSpace(window.location.hostname),
 		overrides?.originAddressSpace ?? guessAddressSpace(hostname),
-		overrides?.targetAddressSpace ?? guessAddressSpace(window.location.hostname)
 	)
 }
 